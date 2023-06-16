3d24dd765cd8392a18db7700f9de8b1682f7370b
Victor <contatoanacletovictor@gmail.com>
2023-06-15 22:57:00 -0300

    Aprendemos includes - fragmentar o codigo usavel

--- recipes/views.py
+++ recipes/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', {
+    return render(request, 'recipes/pages/home.html', {
         'name': 'Luiz Otavio',
     })
